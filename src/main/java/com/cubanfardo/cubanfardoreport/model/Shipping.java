95850059807b5c4c1561696e06ddc01f524cb0ae
CFEXP <reirpinf@gmail.com>
2024-03-20 10:38:23 -0400

    the amount data type is modified

--- src/main/java/com/cubanfardo/cubanfardoreport/model/Shipping.java
+++ src/main/java/com/cubanfardo/cubanfardoreport/model/Shipping.java
@@ -8,7 +8,7 @@ import java.util.List;
 public record Shipping(@NotBlank(message = "Tracking is mandatory") String tracking,
                        @NotBlank(message = "Province is mandatory") String province,
                        @PositiveOrZero(message = "Amount is positive value")
-                       @NotNull(message = "Amount is mandatory") Integer amount,
+                       @NotNull(message = "Amount is mandatory") Double amount,
                        Double weight, String details,
                        @NotNull(message = "Sender is mandatory") Sender sender,
                        @NotNull(message = "Receives is mandatory") Receives receives,
